a82fae3e3b484ce66bc7526367a6caf515076dcd
Krzysztof Kostrzewa <mastermakrela@gmail.com>
2016-09-08 20:02:20 +0200

    Initial Commit

---
+++ BookChallenge/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BookChallenge
+//
+//  Created by Krzysztof Kostrzewa on 08.09.2016.
+//  Copyright © 2016 Krzysztof Kostrzewa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
